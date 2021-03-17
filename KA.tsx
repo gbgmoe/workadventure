626fd3863e8116bbfee4d267d1d5849af36bb90f
gbgmoe <leonhard.moehring@gbg-seelze.eu>
2021-03-17 09:01:04 +0100

    Erdgeschoss

--- KA.tsx
+++ KA.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="KA" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="workadventure-master/workadventure-master/floortileset.png" width="256" height="256"/>
+ <image source="floortileset.png" width="256" height="256"/>
 </tileset>
